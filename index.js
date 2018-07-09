2091c0cc093f9997c582e301ad52bbe74d4651d6
Matthias Osswald <mat.osswald@sap.com>
2018-07-09 12:55:17 +0200

    [FIX] CSP Middleware: Export middleware, add report-uri

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const ui5Server = {
 	server: require("./lib/server"),
 	sslUtil: require("./lib/sslUtil"),
 	middleware: {
+		csp: require("./lib/middleware/csp"),
 		discovery: require("./lib/middleware/discovery"),
 		nonReadRequests: require("./lib/middleware/discovery"),
 		serveIndex: require("./lib/middleware/serveIndex"),
