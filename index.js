8030844e2c5c0eda7b5687319933a1afafd72b47
Merlin Beutlberger <m.beutlberger@sap.com>
2019-06-25 17:18:15 +0200

    Do not export middlewares directly via index.js

--- index.js
+++ index.js
@@ -5,13 +5,5 @@
 module.exports = {
 	server: require("./lib/server"),
 	sslUtil: require("./lib/sslUtil"),
-	middleware: {
-		csp: require("./lib/middleware/csp"),
-		discovery: require("./lib/middleware/discovery"),
-		nonReadRequests: require("./lib/middleware/discovery"),
-		serveIndex: require("./lib/middleware/serveIndex"),
-		serveResources: require("./lib/middleware/serveResources"),
-		serveThemes: require("./lib/middleware/serveThemes"),
-		versionInfo: require("./lib/middleware/versionInfo"),
-	}
+	middlewareRepository: require("./lib/middleware/middlewareRepository")
 };
