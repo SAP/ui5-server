3480105b9601dc1dbd9ed9594b984cf6f66895d5
Merlin Beutlberger <m.beutlberger@sap.com>
2019-07-02 14:41:57 +0200

    Export middlewares in index.js again
    Map to old signature to stay compatible with current, internal consumers

--- index.js
+++ index.js
@@ -5,5 +5,33 @@
 module.exports = {
 	server: require("./lib/server"),
 	sslUtil: require("./lib/sslUtil"),
-	middlewareRepository: require("./lib/middleware/middlewareRepository")
+	middlewareRepository: require("./lib/middleware/middlewareRepository"),
+
+	// Legacy middleware export. Still private.
+	middleware: {
+		csp: require("./lib/middleware/csp"),
+		discovery: mapLegacyMiddlewareArguments(require("./lib/middleware/discovery")),
+		nonReadRequests: mapLegacyMiddlewareArguments(require("./lib/middleware/discovery")),
+		serveIndex: mapLegacyMiddlewareArguments(require("./lib/middleware/serveIndex")),
+		serveResources: mapLegacyMiddlewareArguments(require("./lib/middleware/serveResources")),
+		serveThemes: mapLegacyMiddlewareArguments(require("./lib/middleware/serveThemes")),
+		versionInfo: mapLegacyMiddlewareArguments(require("./lib/middleware/versionInfo")),
+	}
 };
+
+function mapLegacyMiddlewareArguments(module) {
+	// Old arguments was a single object with optional properties
+	// - resourceCollections
+	// - tree
+	return function({resourceCollections, tree} = {}) {
+		const resources = {};
+		resources.all = resourceCollections.combo;
+		resources.rootProject = resourceCollections.source;
+		resources.dependencies = resourceCollections.dependencies;
+
+		return module({
+			resources,
+			tree
+		});
+	};
+}
