90eb278359f442976f96993a15475fd4e347ae9e
Merlin Beutlberger <m.beutlberger@sap.com>
2020-03-30 15:42:51 +0200

    [INTERNAL] index: Stop exporting middleware modules
    They should be retrieved from the middlewareRepository. However, neither
    the modules nor the repository is publicly documented. Therefore this is
    not declared as a breaking change.

--- test/lib/index.js
+++ test/lib/index.js
@@ -5,12 +5,4 @@ test("index.js exports all expected modules", (t) => {
 	t.truthy(index.server, "Module exported");
 	t.truthy(index.sslUtil, "Module exported");
 	t.truthy(index.middlewareRepository, "Module exported");
-
-	t.truthy(index.middleware.csp, "Module exported");
-	t.truthy(index.middleware.discovery, "Module exported");
-	t.truthy(index.middleware.nonReadRequests, "Module exported");
-	t.truthy(index.middleware.serveIndex, "Module exported");
-	t.truthy(index.middleware.serveResources, "Module exported");
-	t.truthy(index.middleware.serveThemes, "Module exported");
-	t.truthy(index.middleware.versionInfo, "Module exported");
 });
