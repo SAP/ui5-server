ef752ac395324230dee146df9b501108fd19ce95
Tobias Sorn <tobias.sorn@sap.com>
2020-05-20 14:37:39 +0200

    [FEATURE] csp: enable writing of reports file
    Reports file is written with the new config option
    "cspReportPath" set. json file written contains
    all csp-reports in an array.

--- test/lib/server/middleware/csp.js
+++ test/lib/server/middleware/csp.js
@@ -1,5 +1,49 @@
 const test = require("ava");
-const cspMiddleware = require("../../../../lib/middleware/csp");
+let cspMiddleware = require("../../../../lib/middleware/csp");
+const mock = require("mock-require");
+
+const sinon = require("sinon");
+const fs = require("graceful-fs");
+
+
+test.before((t) => {
+	// csp-reports file is written async and afterwards a verbose log is done.
+	// wait until the file was written initially and until the first entry was made
+	// using t.context.logVerboseStubCalled promise being called
+	let wasResolved = () => {};
+	t.context.logVerboseStubCalled = new Promise((resolve) => {
+		wasResolved = resolve;
+	});
+
+
+	t.context.logVerboseStub = sinon.stub();
+
+	let callCount = 0;
+	mock("@ui5/logger", {
+		getLogger: () => {
+			return {
+				verbose: (message) => {
+					t.context.logVerboseStub(message);
+					// 2 calls:
+					// * initially creating the file
+					// * adding a csp-violation
+					if (++callCount === 2) {
+						wasResolved();
+					}
+				}
+			};
+		}
+	});
+	mock.reRequire("@ui5/logger");
+	t.context.writeFileStub = sinon.stub(fs, "writeFile").callsArg(2);
+
+	// Re-require to ensure that mocked modules are used
+	cspMiddleware = mock.reRequire("../../../../lib/middleware/csp");
+});
+
+test.after.always(() => {
+	sinon.restore();
+});
 
 test("Default Settings", (t) => {
 	t.plan(3 + 7); // fourth request should end in middleware and not call next!
@@ -36,6 +80,72 @@ test("Default Settings", (t) => {
 	["CONNECT", "DELETE", "HEAD", "OPTIONS", "PATCH", "PUT", "TRACE"].forEach(
 		(method) => middleware({method, url: "/dummy.csplog", headers: {}}, res, next)
 	);
+});
+
+test("Default Settings CSP violation", async (t) => {
+	t.plan(3 + 15); // fourth request should end in middleware and not call next!
+	const middleware = cspMiddleware("sap-ui-xx-csp-policy", {}, "my-report.json");
+	const res = {
+		getHeader: function() {
+			return undefined;
+		},
+		setHeader: function(header, value) {
+			t.fail(`should not be called with header ${header} and value ${value}`);
+		}
+	};
+	const next = function() {
+		t.pass("Next was called.");
+	};
+	const noNext = function() {
+		t.fail("Next should not be called");
+	};
+
+	const cspReport = {
+		"document-uri": "https://otherserver:8080/index.html",
+		"referrer": "",
+		"violated-directive": "script-src-elem",
+		"effective-directive": "script-src-elem",
+		"original-policy": "default-src 'self' myserver:443; report-uri /report-csp-violation",
+		"disposition": "report",
+		"blocked-uri": "inline",
+		"line-number": 17,
+		"source-file": "https://otherserver:8080/index.html",
+		"status-code": 0,
+		"script-sample": ""
+	};
+
+	middleware({method: "GET", url: "/test.html", headers: {}}, res, next);
+	middleware({
+		method: "GET",
+		url: "/test.html?sap-ui-xx-csp-policy=sap-target-level-2",
+		headers: {}
+	}, res, next);
+	middleware({method: "POST", url: "somePath", headers: {}}, res, next);
+	middleware({
+		method: "POST",
+		url: "/dummy.csplog",
+		headers: {"content-type": "application/csp-report"},
+		body: {
+			"csp-report": cspReport
+		}
+	}, res, noNext);
+
+	// check that unsupported methods result in a call to next()
+	["CONNECT", "DELETE", "HEAD", "OPTIONS", "PATCH", "PUT", "TRACE"].forEach(
+		(method) => middleware({method, url: "/dummy.csplog", headers: {}}, res, next)
+	);
+
+	await t.context.logVerboseStubCalled;
+
+
+	t.is(t.context.logVerboseStub.callCount, 2, "should be called 2 times");
+	t.deepEqual(t.context.logVerboseStub.getCall(0).args, ["Wrote csp reports initially to my-report.json"], "first log verbose");
+	t.deepEqual(t.context.logVerboseStub.getCall(1).args, ["Wrote csp reports, length: 1"], "second log verbose");
+	t.is(t.context.writeFileStub.callCount, 2, "should be called 2 times");
+	t.is(t.context.writeFileStub.getCall(0).args[0], "my-report.json", "filename");
+	t.is(t.context.writeFileStub.getCall(0).args[1], "{\"csp-reports\":[]}", "initial content");
+	t.is(t.context.writeFileStub.getCall(1).args[0], "my-report.json", "filename");
+	t.is(t.context.writeFileStub.getCall(1).args[1], "{\"csp-reports\":[" + JSON.stringify(cspReport) + "]}", "content with reports");
 });
 
 test("Custom Settings", (t) => {
