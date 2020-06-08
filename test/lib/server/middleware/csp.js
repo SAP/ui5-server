ebd252d93ba7b52c9bc886fe6c2e6abe7034f938
Matthias Osswald <mat.osswald@sap.com>
2020-06-08 11:42:30 +0200

    Change HTML middleware back to "use" / fix tests (async)

--- test/lib/server/middleware/csp.js
+++ test/lib/server/middleware/csp.js
@@ -2,30 +2,34 @@ const test = require("ava");
 const cspMiddleware = require("../../../../lib/middleware/csp");
 
 
-test("OPTIONS request", (t) => {
+test("OPTIONS request", async (t) => {
 	t.plan(2);
 	const middleware = cspMiddleware("sap-ui-xx-csp-policy", {});
-	const res = {
-		getHeader: function() {
-			return undefined;
-		},
-		end: function() {
-			t.true(true, "end is called");
-		},
-		setHeader: function(header, value) {
-			if (header.startsWith("Content-Security-Policy")) {
-				t.fail(`should not be called with header ${header} and value ${value}`);
-			}
-			if (header === "Allow") {
-				t.is(value, "GET, HEAD, POST", "GET, HEAD, POST are set");
-			}
-		}
-	};
-	const next = function() {
-		t.fail("should not be called.");
-	};
 
-	middleware({method: "OPTIONS", url: "/.ui5/csp/report.csplog", headers: {}}, res, next);
+	await new Promise((resolve) => {
+		const res = {
+			getHeader: function() {
+				return undefined;
+			},
+			end: function() {
+				t.true(true, "end is called");
+				resolve();
+			},
+			setHeader: function(header, value) {
+				if (header.startsWith("Content-Security-Policy")) {
+					t.fail(`should not be called with header ${header} and value ${value}`);
+				}
+				if (header === "Allow") {
+					t.is(value, "POST", "POST should be allowed");
+				}
+			}
+		};
+		const next = function() {
+			t.fail("should not be called.");
+			resolve();
+		};
+		middleware({method: "OPTIONS", url: "/.ui5/csp/report.csplog", headers: {}}, res, next);
+	});
 });
 
 test("Default Settings", async (t) => {
@@ -101,34 +105,42 @@ test("Default Settings CSP violation", async (t) => {
 		"script-sample": ""
 	};
 
-	const res = {
-		writeHead: function(status, contentType) {
-		},
-		end: function(content) {
-			t.is(content, JSON.stringify({"csp-reports": [cspReport]}, null, "\t"), "content matches");
-		},
-	};
+	await new Promise((resolve) => {
+		const noNext = function() {
+			t.fail("Next should not be called");
+			resolve();
+		};
+		middleware({
+			method: "POST",
+			url: "/.ui5/csp/report.csplog",
+			headers: {"content-type": "application/csp-report"},
+			body: {
+				"csp-report": cspReport
+			}
+		}, {
+			end: resolve
+		}, noNext);
+	});
 
-	const noNext = function() {
-		t.fail("Next should not be called");
-	};
-
-	middleware({
-		method: "POST",
-		url: "/.ui5/csp/report.csplog",
-		headers: {"content-type": "application/csp-report"},
-		body: {
-			"csp-report": cspReport
-		}
-	}, {
-		end: () => {
-			middleware({
-				method: "GET",
-				url: "/.ui5/csp/csp-reports.json",
-				headers: {"content-type": "application/json"}
-			}, res, noNext);
-		}
-	}, noNext);
+	await new Promise((resolve) => {
+		const noNext = function() {
+			t.fail("Next should not be called");
+			resolve();
+		};
+		const res = {
+			writeHead: function(status, contentType) {
+			},
+			end: function(content) {
+				t.is(content, JSON.stringify({"csp-reports": [cspReport]}, null, "\t"), "content matches");
+				resolve();
+			},
+		};
+		middleware({
+			method: "GET",
+			url: "/.ui5/csp/csp-reports.json",
+			headers: {"content-type": "application/json"}
+		}, res, noNext);
+	});
 });
 
 
@@ -138,21 +150,23 @@ test("Default Settings CSP violation without body parser, invalid body content",
 		serveCSPReports: true
 	});
 
-
-	const nextFunction = function(error) {
-		t.true(error instanceof Error);
-		t.is(error.message, "No body content available: /.ui5/csp/report.csplog", "error message matches");
-	};
-
-	middleware({
-		method: "POST",
-		url: "/.ui5/csp/report.csplog",
-		headers: {"content-type": "application/csp-report"},
-		body: "test"
-	}, {
-		end: function() {
-			t.fail("res.end should not be called");
-		}}, nextFunction);
+	await new Promise((resolve) => {
+		const nextFunction = function(error) {
+			t.true(error instanceof Error);
+			t.is(error.message, "No body content available: /.ui5/csp/report.csplog", "error message matches");
+			resolve();
+		};
+		middleware({
+			method: "POST",
+			url: "/.ui5/csp/report.csplog",
+			headers: {"content-type": "application/csp-report"},
+			body: "test"
+		}, {
+			end: function() {
+				t.fail("res.end should not be called");
+				resolve();
+			}}, nextFunction);
+	});
 });
 
 
@@ -190,49 +204,62 @@ test("Default Settings two CSP violations", async (t) => {
 		"script-sample": ""
 	};
 
-	const res = {
-		writeHead: function(status, contentType) {
-		},
-		end: function(content) {
-			t.is(content, JSON.stringify({"csp-reports": [cspReport1, cspReport2]}, null, "\t"), "content matches");
-		},
-	};
+	await new Promise((resolve) => {
+		middleware({
+			method: "POST",
+			url: "/.ui5/csp/report.csplog",
+			headers: {"content-type": "application/csp-report"},
+			body: {
+				"csp-report": cspReport1
+			}
+		}, {
+			end: function() {
+				t.true(true, "end is called");
+				resolve();
+			}
+		}, () => {
+			t.fail("Next should not be called");
+			resolve();
+		});
+	});
 
-	const noNext = function() {
-		t.fail("Next should not be called");
-	};
+	await new Promise((resolve) => {
+		middleware({
+			method: "POST",
+			url: "/.ui5/csp/report.csplog",
+			headers: {"content-type": "application/csp-report"},
+			body: {
+				"csp-report": cspReport2
+			}
+		}, {
+			end: function() {
+				t.true(true, "end is called");
+				resolve();
+			}
+		}, () => {
+			t.fail("Next should not be called");
+			resolve();
+		});
+	});
 
-	middleware({
-		method: "POST",
-		url: "/.ui5/csp/report.csplog",
-		headers: {"content-type": "application/csp-report"},
-		body: {
-			"csp-report": cspReport1
-		}
-	}, {
-		end: function() {
-			t.true(true, "end is called");
-		}
-	}, noNext);
-
-	middleware({
-		method: "POST",
-		url: "/.ui5/csp/report.csplog",
-		headers: {"content-type": "application/csp-report"},
-		body: {
-			"csp-report": cspReport2
-		}
-	}, {
-		end: function() {
-			t.true(true, "end is called");
-		}
-	}, noNext);
-
-	middleware({
-		method: "GET",
-		url: "/.ui5/csp/csp-reports.json",
-		headers: {"content-type": "application/json"}
-	}, res, noNext);
+	await new Promise((resolve) => {
+		const res = {
+			writeHead: function(status, contentType) {
+			},
+			end: function(content) {
+				t.is(content, JSON.stringify({"csp-reports": [cspReport1, cspReport2]}, null, "\t"), "content matches");
+				resolve();
+			},
+		};
+		middleware({
+			method: "GET",
+			url: "/.ui5/csp/csp-reports.json",
+			headers: {"content-type": "application/json"}
+		}, res, () => {
+			t.fail("Next should not be called");
+			resolve();
+		});
+	});
 });
 
 test("Default Settings no CSP violations", async (t) => {
@@ -241,26 +268,29 @@ test("Default Settings no CSP violations", async (t) => {
 		serveCSPReports: true
 	});
 
-	const res = {
-		writeHead: function(status, contentType) {
-		},
-		end: function(content) {
-			t.is(content, JSON.stringify({"csp-reports": []}, null, "\t"), "content matches");
-		},
-	};
+	await new Promise((resolve) => {
+		const res = {
+			writeHead: function(status, contentType) {
+			},
+			end: function(content) {
+				t.is(content, JSON.stringify({"csp-reports": []}, null, "\t"), "content matches");
+				resolve();
+			},
+		};
 
-	const noNext = function() {
-		t.fail("Next should not be called");
-	};
-
-	middleware({
-		method: "GET",
-		url: "/.ui5/csp/csp-reports.json",
-		headers: {"content-type": "application/json"}
-	}, res, noNext);
+		const noNext = function() {
+			t.fail("Next should not be called");
+			resolve();
+		};
+		middleware({
+			method: "GET",
+			url: "/.ui5/csp/csp-reports.json",
+			headers: {"content-type": "application/json"}
+		}, res, noNext);
+	});
 });
 
-test("Custom Settings", (t) => {
+test("Custom Settings", async (t) => {
 	const middleware = cspMiddleware("csp", {
 		definedPolicies: {
 			policy1: "default-src 'self';",
@@ -287,21 +317,33 @@ test("Custom Settings", (t) => {
 			}
 		}
 	};
-	const next = function() {
-		t.pass("Next was called.");
-	};
 
 	expected = ["default-src 'self';", "default-src http:;"];
-	middleware({method: "GET", url: "/test.html", headers: {}}, res, next);
+	await new Promise((resolve) => {
+		middleware({method: "GET", url: "/test.html", headers: {}}, res, () => {
+			t.pass("Next was called.");
+			resolve();
+		});
+	});
 
 	expected = ["default-src https:;", "default-src http:;"];
-	middleware({method: "GET", url: "/test.html?csp=policy3", headers: {}}, res, next);
+	await new Promise((resolve) => {
+		middleware({method: "GET", url: "/test.html?csp=policy3", headers: {}}, res, () => {
+			t.pass("Next was called.");
+			resolve();
+		});
+	});
 
 	expected = ["default-src ftp:;", "default-src http:;"];
-	middleware({method: "GET", url: "/test.html?csp=default-src%20ftp:;", headers: {}}, res, next);
+	await new Promise((resolve) => {
+		middleware({method: "GET", url: "/test.html?csp=default-src%20ftp:;", headers: {}}, res, () => {
+			t.pass("Next was called.");
+			resolve();
+		});
+	});
 });
 
-test("No Dynamic Policy Definition", (t) => {
+test("No Dynamic Policy Definition", async (t) => {
 	const middleware = cspMiddleware("csp", {
 		definedPolicies: {
 			policy1: "default-src 'self';",
@@ -325,15 +367,17 @@ test("No Dynamic Policy Definition", (t) => {
 			}
 		}
 	};
-	const next = function() {
-		t.pass("Next was called.");
-	};
 
 	const expected = ["default-src 'self';", "default-src http:;"];
-	middleware({method: "GET", url: "/test.html?csp=default-src%20ftp:;", headers: {}}, res, next);
+	await new Promise((resolve) => {
+		middleware({method: "GET", url: "/test.html?csp=default-src%20ftp:;", headers: {}}, res, () => {
+			t.pass("Next was called.");
+			resolve();
+		});
+	});
 });
 
-test("Header Manipulation, add headers to existing header", (t) => {
+test("Header Manipulation, add headers to existing header", async (t) => {
 	t.plan(3);
 	const middleware = cspMiddleware("csp", {
 		definedPolicies: {
@@ -359,8 +403,11 @@ test("Header Manipulation, add headers to existing header", (t) => {
 			}
 		}
 	};
-	const next = function() {};
 
-	middleware({method: "GET", url: "/test.html", headers: {}}, res, next);
-	t.deepEqual(cspHeader, ["default-src: spdy:", "default-src 'self';", "default-src http:;"]);
+	await new Promise((resolve) => {
+		middleware({method: "GET", url: "/test.html", headers: {}}, res, () => {
+			t.deepEqual(cspHeader, ["default-src: spdy:", "default-src 'self';", "default-src http:;"]);
+			resolve();
+		});
+	});
 });
