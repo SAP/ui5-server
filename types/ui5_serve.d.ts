e872904836a5f9f3b0b6c49696552933b5b9f795
Yavor Ivanov <yavor.ivanov@sap.com>
2023-10-17 15:33:19 +0300

    Add comments

--- types/ui5_serve.d.ts
+++ types/ui5_serve.d.ts
@@ -3,5 +3,6 @@ declare module "@ui5/server" {
 
 	function serve(graph: ProjectGraph, configuration: Record<string, string | number | boolean>): Function;
 
+	// This one should be (eventually) provided globally or as a part of @ui5/server/Specification 
 	type availableSpecVersions = "2.0" | "2.2" | "3.0" | "3.2";
 }
