5a41451d4f5e22b4a51a7edf6d7d13f2642551af
Yavor Ivanov <yavor.ivanov@sap.com>
2023-10-18 14:12:35 +0300

    Scope imports

--- types/ui5_serve.d.ts
+++ types/ui5_serve.d.ts
@@ -1,5 +1,5 @@
-import "@ui5/project/types"
 declare module "@ui5/server" {
+	import "@ui5/project/types"
 	import { ProjectGraph } from "@ui5/project/graph";
 
 	function serve(graph: ProjectGraph, configuration: Record<string, string | number | boolean>): Function;
